2765243c40abb3a0ae8d22da2636db95ce64c47d
Izabela Walczak <iza.niznik@gmail.com>
2018-08-27 13:51:00 +0200

    --New message

---
+++ library.php
@@ -0,0 +1,47 @@
+<?php
+ini_set('display_errors', 1);
+
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+
+class Library{
+  public $title;
+  public $author;
+  public $releaseYear;
+
+
+  function __construct($title, $author, $releaseYear) {
+    $this->setTitle($title);
+    $this->setAuthor($author);
+    $this->setreleaseYear($releaseYear);
+  }
+
+  public function setTitle($title) {
+    $this->title = $title;
+  }
+  public function setAuthor($author) {
+    $this->author = $author;
+  }
+  public function setreleaseYear($releaseYear) {
+    $this->releaseYear = $releaseYear;
+  }
+  public function getTitle() {
+    return $this->title;
+  }
+  public function getAuthor() {
+    return $this->author;
+  }
+  public function getreleaseYear() {
+    return $this->releaseYear;
+  }
+
+
+
+}
+$vara = new Library("Hej","Adam", 1891);
+echo "<br />Title: " . $vara->getTitle();
+echo "<br />Author: " . $vara->getAuthor();
+echo "<br />ReleaseYear: " . $vara->getreleaseYear();
+
+?>
